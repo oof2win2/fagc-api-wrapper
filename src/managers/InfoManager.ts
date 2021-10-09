88b4d82a4f57741afac4b62e6574591969dce907
oof2win2 <honza.koco44@gmail.com>
2021-10-10 01:02:13 +0200

    :wrench: Notify guild with embed or text

--- src/managers/InfoManager.ts
+++ src/managers/InfoManager.ts
@@ -2,14 +2,18 @@ import fetch from "isomorphic-fetch"
 import { ManagerOptions, WrapperOptions } from "../types/types"
 import { Webhook } from "fagc-api-types"
 import BaseManager from "./BaseManager"
-import { GenericAPIError } from "../types"
+import { GenericAPIError, RequestConfig, NoApikeyError } from "../types"
+import strictUriEncode from "strict-uri-encode"
+import { APIEmbed } from "discord-api-types"
 
 export default class InfoManager extends BaseManager<Webhook> {
 	public apikey?: string
+	public masterapikey?: string
 	private apiurl: string
 	constructor(options: WrapperOptions, managerOptions: ManagerOptions = {}) {
 		super(managerOptions)
 		if (options.apikey) this.apikey = options.apikey
+		if (options.masterapikey) this.masterapikey = options.masterapikey
 		this.apiurl = options.apiurl
 	}
 	async addWebhook(
@@ -40,5 +44,50 @@ export default class InfoManager extends BaseManager<Webhook> {
 			headers: { "content-type": "application/json" },
 		}).then((w) => w.json())
 		return add
+	}
+
+	async notifyGuildText(
+		guildId: string,
+		text: string,
+		reqConfig: RequestConfig = {}
+	): Promise<void> {
+		if (!this.masterapikey && !reqConfig.masterapikey)
+			throw new NoApikeyError()
+
+		await fetch(
+			`${this.apiurl}/informatics/notify/${strictUriEncode(guildId)}`,
+			{
+				method: "POST",
+				body: JSON.stringify({
+					data: text,
+				}),
+				headers: {
+					authorization: `Token ${reqConfig.apikey || this.apikey}`,
+					"content-type": "application/json",
+				},
+			}
+		)
+	}
+	async notifyGuildEmbed(
+		guildId: string,
+		embed: APIEmbed,
+		reqConfig: RequestConfig = {}
+	): Promise<void> {
+		if (!this.masterapikey && !reqConfig.masterapikey)
+			throw new NoApikeyError()
+
+		await fetch(
+			`${this.apiurl}/informatics/notify/${strictUriEncode(
+				guildId
+			)}/embed`,
+			{
+				method: "POST",
+				body: JSON.stringify(embed),
+				headers: {
+					authorization: `Token ${reqConfig.apikey || this.apikey}`,
+					"content-type": "application/json",
+				},
+			}
+		)
 	}
 }
