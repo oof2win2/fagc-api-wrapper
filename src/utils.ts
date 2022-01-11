3a0b2560cb3b3cf677d2a9518ed276d7954381b9
oof2win2 <honza.koco44@gmail.com>
2022-01-11 08:57:46 +0100

    🐛 fix authenticate documentation

--- src/utils.ts
+++ src/utils.ts
@@ -4,10 +4,9 @@ import BaseManager from "./managers/BaseManager"
 
 /**
  * A decorator that checks whether an api key is set in the reqConfig object or on the class, if it isnt it throws an error.
- * Sets the `reqConfig.apikey` to be the value of the api key from the original `reqConfig` or the class.
  * 
  * @throws NoAuthError if the API key is not set
- * @returns String to use for the Authorization header
+ * @returns String to use for the Authorization header, created from original `reqConfig` or the class
  */
 export const authenticate = <X, T extends Common>(manager: X & BaseManager<T>, reqConfig: RequestConfig): string => {
 	const { apikey } = reqConfig || manager
@@ -16,11 +15,10 @@ export const authenticate = <X, T extends Common>(manager: X & BaseManager<T>, r
 }
 
 /**
- * A decorator that checks whether an api key is set in the reqConfig object or on the class, if it isnt it throws an error.
- * Sets the `reqConfig.apikey` to be the value of the api key from the original `reqConfig` or the class.
+ * A function that checks whether an api key is set in the reqConfig object or on the class, if it isnt it throws an error.
  * 
  * @throws NoAuthError if the API key is not set
- * @returns String to use for the Authorization header
+ * @returns String to use for the Authorization header, created from original `reqConfig` or the class
  */
 export const masterAuthenticate = <X, T extends Common>(manager: X & BaseManager<T>, reqConfig: RequestConfig): string => {
 	const { masterapikey } = reqConfig || manager
