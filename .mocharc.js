06bee9c42977bd97be0560c2bf3b358ae8e546be
oof2win2 <honza.koco44@gmail.com>
2022-01-15 22:55:28 +0100

    🐴 make tests faster by 10s (50%)

--- .mocharc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	reporter: "nyan",
-	exit: true,
-	require: ["ts-node/register", "mocha-steps"],
-	spec: "./test/*.test.ts",
-}
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-	preset: "ts-jest"
+	preset: "ts-jest",
+	globals: {
+		"ts-jest": {
+			isolatedModules: true,
+			tsconfig: "tsconfig.test.json"
+		}
+	},
+	maxWorkers: 1,
 }
